c2fb2aad30c3ff1b4dfbaa276c449d8cfdfe5f33
lapenkovdaniil <daniil.adst@gmail.com>
2018-09-26 19:23:42 +0300

    1 lesson

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -1,5 +1,4 @@
 @file:Suppress("UNUSED_PARAMETER")
-package lesson1.task1
 
 import kotlin.math.*
 
@@ -58,7 +57,12 @@ fun main(args: Array<String>) {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
+
+    val h = hours * 3600
+    val m = minutes * 60
+    return seconds + m + h
+}
 
 /**
  * Тривиальная
@@ -67,7 +71,13 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
+    val s = sagenes * 48 * 4.445
+    val a = arshins * 48 / 3 * 4.445
+    val v = vershoks * 4.445
+    return (s + a + v) / 100
+
+}
 
 /**
  * Тривиальная
@@ -75,15 +85,22 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = TODO()
-
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double {
+    val m = min / 60.0
+    val s = sec / 3600.0
+    return (m + s + grad) * PI / 180.0
+}
 /**
  * Тривиальная
  *
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
+    val x = x2 - x1
+    val y = y2 - y1
+    return sqrt(x * x + y * y)
+}
 
 /**
  * Простая
@@ -91,7 +108,10 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int {
+    val n = number % 1000
+    return n / 100
+}
 
 /**
  * Простая
@@ -100,8 +120,11 @@ fun thirdDigit(number: Int): Int = TODO()
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
-
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
+    val ha = hoursArrive * 60 + minutesArrive
+    val hd = hoursDepart * 60 + minutesDepart
+    return ha - hd
+}
 /**
  * Простая
  *
@@ -109,7 +132,11 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int): Double {
+    val i = initial * (1 + percent * 0.01 * 3.0)
+    return initial * i
+}
+
 
 /**
  * Простая
@@ -117,4 +144,9 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int {
+    val a = (number / 100)
+    val b = (number - a * 100) / 10
+    val c = (number - 10 * b - 100 * a)
+    return a + b * 10 + c * 100
+}
